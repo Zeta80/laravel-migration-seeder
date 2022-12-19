79182894f49ce2750f7d66e0e7341ef911e06ff3
Mirko Robbi <mirko.robbi@gmail.com>
2022-12-19 15:51:43 +0100

    add train stamp in page

--- resources/views/trains/train.blade.php
+++ resources/views/trains/train.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    ciao
+    <div class="container">
+        <h2>Treni in partenza... CIUF CIUF</h2>
+        <ul>
+            @foreach ($trains as $train)
+                <h3>treno numero: {{ $train->codice_treno }}</h3>
+                <li>andata:{{ $train->stazione_partenza }}, {{ $train->giorno_partenza }}, {{ $train->orario_partenza }}
+                </li>
+                <li>arrivo:{{ $train->stazione_arrivo }}, {{ $train->giorno_arrivo }}, {{ $train->orario_arrivo }}</li>
+                <li>numero carrozze:{{ $train->numero_carrozze }}</li>
+                <li>finanziato da: {{ $train->azienda }}</li>
+            @endforeach
+        </ul>
+    </div>
 @endsection
